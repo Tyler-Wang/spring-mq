1b27477b5568c12fb2176a6b8d3e7b674cc95d3f
Tyler-Wang <983243283@qq.com>
2017-12-09 17:50:59 +0800

    complete kafka

--- src/main/java/com/hunger/sample/springmq/kafka/Client.java
+++ src/main/java/com/hunger/sample/springmq/kafka/Client.java
@@ -1,7 +1,31 @@
 package com.hunger.sample.springmq.kafka;
 
+import java.net.URL;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hunger.sample.springmq.common.TestMqRequest;
+import com.hunger.sample.springmq.kafka.producer.KafkaMessageProducer;
+
 public class Client {
 	public static void main(String[] args) {
+		URL url = Client.class.getResource("/conf/log4j.properties");
+		String path = url.getPath();
+		PropertyConfigurator.configureAndWatch(path);
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/spring-*.xml");
 		
+		KafkaMessageProducer kmp = ctx.getBean(KafkaMessageProducer.class);
+		while(true){
+			TestMqRequest req = new TestMqRequest();
+			req.setMsgId(""+System.currentTimeMillis());
+			kmp.sendQueueMessage(req);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- src/main/java/com/hunger/sample/springmq/kafka/producer/KafkaMessageProducer.java
+++ src/main/java/com/hunger/sample/springmq/kafka/producer/KafkaMessageProducer.java
@@ -1,9 +1,12 @@
 package com.hunger.sample.springmq.kafka.producer;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
 import com.hunger.sample.springmq.common.IMqMessageProducer;
 import com.hunger.sample.springmq.common.MqMessage;
+import com.hunger.sample.springmq.utils.JdkSerializeUtil;
 
 public class KafkaMessageProducer implements IMqMessageProducer {
 
@@ -13,15 +16,23 @@ public class KafkaMessageProducer implements IMqMessageProducer {
 	@Override
 	public void sendQueueMessage(MqMessage mqMessage) {
 		// TODO Auto-generated method stub
-		
+		this.send(mqMessage);
 	}
 
 	@Override
 	public void sendTopicMessage(MqMessage mqMessage) {
 		// TODO Auto-generated method stub
-
+		this.send(mqMessage);
 	}
 
+	@SuppressWarnings("unchecked")
+	private ListenableFuture<SendResult<String, String>> send(MqMessage mqMessage){
+		String topic = mqMessage.getQueue().getQueueName();
+		byte[] data = JdkSerializeUtil.serialize(mqMessage);
+		return kafkaTemplate.send(topic, data);
+	}
+	
+	@SuppressWarnings("rawtypes")
 	public KafkaTemplate getKafkaTemplate() {
 		return kafkaTemplate;
 	}
